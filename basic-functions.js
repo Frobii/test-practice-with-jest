a3c891cadb51494536c05520ece11f262c4fab85
frobee <frobi.webdev@outlook.com>
2023-09-27 20:32:24 +0930

    build-and-test-basic-functions-including-a-caesar-cipher

--- basic-functions.js
+++ basic-functions.js
@@ -3,8 +3,80 @@ const testFunctions = () => {
         return string[0].toUpperCase() + string.substring(1)
     }
 
+    function reverse(string) {
+        let reversedString = [];
+        for (let i = string.length; i > 0; i -= 1) {
+            reversedString.push(string[i - 1])
+        }
+        return reversedString.join('');
+    }
+
+    const calculator = () => {
+        function add(a,b) {
+            return a + b
+        }
+        function subtract(a,b) {
+            return a - b
+        }
+        function multiply(a,b) {
+            return a * b
+        }
+        function divide(a,b) {
+            return a / b
+        }
+        return {
+            add,
+            subtract,
+            multiply,
+            divide,
+        }
+    }
+
+    const caesarCipher = () => {
+        function isAlphabetical(char) {
+            const regex = /^[a-z]+$/;
+            return regex.test(char.toLowerCase());
+        };
+        function isCapitalized(char) {
+            if (char.toUpperCase() === char) {
+                return true
+            } else {
+                return false
+            };
+        };
+        function encodeCharacter(char, num) {
+            const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+            const charPosition = alphabet.indexOf(char.toLowerCase());
+            const encodedCharPosition = (num + charPosition) % 26;
+            const encodedCharacter = alphabet[encodedCharPosition];
+            if (isCapitalized(char)) {
+                return encodedCharacter.toUpperCase()
+            } else {
+                return encodedCharacter
+            };
+        };
+        function encodeString(string, num) {
+            let encodedString = ''
+            for (const char of string) {
+                if (isAlphabetical(char)) {
+                    const encodedChar = encodeCharacter(char,num)
+                    encodedString += encodedChar
+                } else { 
+                    encodedString += char
+                }
+            }
+            return encodedString;
+        };
+        return {
+            encodeString,
+        };
+    }
+
     return {
         capitalize,
+        reverse,
+        calculator,
+        caesarCipher,
     }
 }
 
--- basic-functions.test.js
+++ basic-functions.test.js
@@ -4,3 +4,38 @@ test('Capitlizes the first letter of a string', () => {
     expect(testFunctions().capitalize('hello!')).toBe('Hello!');
 })
 
+test('Reverses a string', () => {
+    expect(testFunctions().reverse('!dlroW olleH')).toBe('Hello World!')
+})
+
+test('Calculator can add', () => {
+    expect(testFunctions().calculator().add(1, 10)).toEqual(11)
+})
+
+test('Calculator can subtract', () => {
+    expect(testFunctions().calculator().subtract(10, 1)).toEqual(9)
+})
+
+test('Calculator can multiply', () => {
+    expect(testFunctions().calculator().multiply(10, 11)).toEqual(110)
+})
+
+test('Calculator can divide', () => {
+    expect(testFunctions().calculator().divide(90, 10)).toEqual(9)
+})
+
+test('Caesar cipher can encode a string which wraps the alphabet', () => {
+    expect(testFunctions().caesarCipher().encodeString('zyzz',3)).toBe('cbcc')
+})
+
+test('Caesar cipher can encode a string which wraps the alphabet', () => {
+    expect(testFunctions().caesarCipher().encodeString('zyzz',3)).toBe('cbcc')
+})
+
+test('Caesar cipher maintains case', () => {
+    expect(testFunctions().caesarCipher().encodeString('ZyZZ',3)).toBe('CbCC')
+})
+
+test('Caesar cipher maintains punctuation', () => {
+    expect(testFunctions().caesarCipher().encodeString('!!! Hellooo Wooorld !!!', 11)).toBe('!!! Spwwzzz Hzzzcwo !!!')
+})
